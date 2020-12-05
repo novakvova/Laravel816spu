3d277f759189ce36e6831809b706b4d784dbb2cd
Volodymyr Novak <novakvova@gmail.com>
2020-12-05 12:32:37 +0200

    Complete tabels database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             CategorySeeder::class,
-            TagSeeder::class
+            TagSeeder::class,
+            PostSeeder::class,
+            TagsPostSeeder::class
         ]);
     }
 }
